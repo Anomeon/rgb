55efaecd305950e2f570dc54d367d3e5a4005f0a
Anomeon <danuy@list.ru>
2014-02-15 19:20:38 +0400

    добавил условие ИЛИ

--- getBackgroundColor.macros.js
+++ getBackgroundColor.macros.js
@@ -5,10 +5,10 @@ function myFunction() {
   var cellArray = [];
   var ss = SpreadsheetApp.getActiveSpreadsheet();
   var sheet = ss.getActiveSheet();
-  for (var i = 1; i < 2448; i++) {
-    var range = sheet.getRange(i,1);
+  for (var i = 1; i < 3647; i++) {
+    var range = sheet.getRange(i, 1);
     var color = range.getBackground();
-    if (color == '#FFF2CC') {
+    if (color == '#FFF2CC' || color == '#fff2cc') {
       cellArray.push(range.getRow());
     }
   }
